eb89b2e2c665bc600ff03b0e66d5e1401f3c4adb
Douglas Fortunato <douglasfortunato@hotmail.com>
2019-11-21 09:57:19 -0300

    Init commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const a = 3;
+const b = 2;
+const e = 4;
+const c = "sdaasdasdasdadasdasdasdasdasdasdbasjdhbasdbashdgasdghashdgashdgasjdahsdgasjdghashdgashdgashgahsdgashdgashdgashdghsgdasdhashashashdgashdahsahsdahsgahsdgahsdgsghahj";
