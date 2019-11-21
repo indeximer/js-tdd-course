09da732e2ca18c6f95c83cdf20226e05d13a0d27
Douglas Fortunato <douglasfortunato@hotmail.com>
2019-11-21 10:12:21 -0300

    Fix js style

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-a = 3;
-const b = 2
-const e = 4;
-const c = "sdaasdasdasdadasdasdasdasdasdasdbasjdhbasdbashdgasdghashdgashdgasjdahsdgasjdghashdgashdgashgahsdgashdgashdgashdghsgdasdhashashashdgashdahsahsdahsgahsdgahsdgsghahj";
+const a = 3;
+const b = 2;
+
+function sum(num1, num2) {
+  const total = num1 + num2;
+  return total;
+}
+
+sum(a, b);
